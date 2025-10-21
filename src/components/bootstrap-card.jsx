10fee8eb5fbbcd19b652b4e5f1420799edff7b10
Md Faisal Yousuf Afrid <mdfaisalafrid@gmail.com>
2025-10-21 23:18:27 +0600

    transion added on img

--- src/components/bootstrap-card.jsx
+++ src/components/bootstrap-card.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Bootstracard(props) {
   const { src } = props;
   return (
-    <Card className='p-3'>
-      <Card.Img variant="top" src={src} className='img-fluid w-100 h-100 object-fit-cover' />
-      <Card.Body className='px-0 pb-0'>
+    <Card className="p-3">
+      <div className="img-content overflow-hidden p-0">
+        <Card.Img
+          variant="top"
+          src={src}
+          className="img-fluid animate-img w-100 h-100 object-fit-cover"
+        />
+      </div>
+      <Card.Body className="px-0 pb-0">
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Button className='btn btn-info'>Go somewhere</Button>
+        <Button className="btn btn-info">Go somewhere</Button>
       </Card.Body>
     </Card>
-  )
+  );
 }

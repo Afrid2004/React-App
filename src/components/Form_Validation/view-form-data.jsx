8d9ac8bc058171d1475914ddacf44fe725fc705f
afrid2004 <mdfaisalafrid@gmail.com>
2025-11-19 00:45:35 +0600

    yup added

--- src/components/Form_Validation/view-form-data.jsx
+++ src/components/Form_Validation/view-form-data.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 const ViewFormData = (props) => {
   const { data } = props;
   const { name, email } = data;
+
   console.log(data)
   return (
     <div className='card' style={{ minWidth: "450px" }}>
       <h2 className='mb-3'>User Details</h2>
-      <h5 className='mb-3'>Hello, {name}</h5>
+      <h5 className='mb-3'>Hello, {name.split(" ")[0]}</h5>
       <div>
         <h6 className='mb-3'>User : {name}</h6>
         <p>Email : {email}</p>

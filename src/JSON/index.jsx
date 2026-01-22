8084ca6946150b17753cb910490da04a20b55bb0
afrid2004 <mdfaisalafrid@gmail.com>
2026-01-22 09:28:53 +0600

    json server added

--- src/JSON/index.jsx
+++ src/JSON/index.jsx
@@ -3,9 +3,18 @@ import React, { useEffect, useState } from 'react'
 
 const JsonServer = () => {
   const [data, setData] = useState([]);
+  const [error, setError] = useState('');
+  const [load, setLoad] = useState(true);
   const getData = async () => {
-    const response = await axios.get('http://localhost:3000/user');
-    setData(response.data);
+    setLoad(true);
+    try{
+      const response = await axios.get('http://localhost:3000/user');
+      setData(response.data);
+      setLoad(false);
+    }catch{
+      setError('Failed to fetch Data. May be the url is not ok. Please Check and try again.');
+      setLoad(false);
+    }
   }
   useEffect(()=> {
     getData();
@@ -26,6 +35,7 @@ const JsonServer = () => {
       </div>
       })}
       </div>
+      <p>{ load && 'Data is loading...' }{error && error}</p>
     </div>
   )
 }
